31b3e2a4347bb7c383d9b5b78f2dc59ae8210760
thanujsai <sboda@modeln.com>
2025-03-17 20:46:01 +0530

    layouts

---
+++ resources/js/Layouts/Layout.jsx
@@ -0,0 +1,19 @@
+import { Link } from '@inertiajs/react';
+import React from 'react';
+
+function Layout({ children }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <header className="bg-black text-white p-4 shadow-md">
+        <nav className="flex justify-center space-x-6">
+          <Link className="nav-link text-lg font-semibold hover:text-yellow-200 transition" href="/">Home</Link>{/* if we use a tag it reload the entire page whenever clicked, whereas link tag only reloads the component */}
+          <Link className="nav-link text-lg font-semibold hover:text-yellow-200 transition" href="/about">About</Link>
+        </nav>
+      </header>
+
+      <main className="flex-1 p-6">{children}</main>
+    </div>
+  );
+}
+
+export default Layout;
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,13 +1,16 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createInertiaApp } from '@inertiajs/react'
-import { createRoot } from 'react-dom/client'
+import { createInertiaApp } from '@inertiajs/react';
+import { createRoot } from 'react-dom/client';
+import Layout from '@/Layouts/Layout';
 
 createInertiaApp({
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
-    return pages[`./Pages/${name}.jsx`]
+    let page = pages[`./Pages/${name}.jsx`]
+    page.default.layout = page.default.layout || ((page) => <Layout children={page} />);//if the page has default layout use that one, otherwise use our layout
+    return page;
   },
   setup({ el, App, props }) {
     createRoot(el).render(<App {...props} />)
